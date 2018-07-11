20b2d17faa484d552007b059e2d28ff0c623b1ee
baituo <baituo@huanleguang.com>
2018-07-11 16:08:34 +0800

    fix: modify static variable

--- src/utils/control-point.js
+++ src/utils/control-point.js
@@ -1,10 +1,12 @@
+const pointStyle = {
+  control_point_radius: 5,
+  control_point_color: '#00ffff'
+}
 export class ControlPoint {
   constructor(x, y) {
     this.x = x || 0
     this.y = y || 0
   }
-  static CONTROL_POINT_RADIUS = 5
-  static CONTROL_POINT_COLOR = '#00ffff'
   
   ctx() {
     return ctx
@@ -14,13 +16,13 @@ export class ControlPoint {
   draw(ratio) {
     ratio = ratio || 1
     this.ctx.beginPath()
-    this.ctx.arc(this.x * ratio, this.y * ratio, CONTROL_POINT_RADIUS, 0, Math.PI * 2, false)
+    this.ctx.arc(this.x * ratio, this.y * ratio, pointStyle.control_point_radius, 0, Math.PI * 2, false)
   }
   print(ratio) {
     this.draw(ratio)
     this.ctx.save()
-    this.ctx.strokeStyle = CONTROL_POINT_COLOR
-    this.ctx.fillStyle = CONTROL_POINT_COLOR
+    this.ctx.strokeStyle = pointStyle.control_point_color
+    this.ctx.fillStyle = pointStyle.control_point_color
     this.ctx.stroke()
     this.ctx.fill()
     this.ctx.restore()
