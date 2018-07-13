92c2da20035ada88c771479fddf94f1a4ee369aa
baituo <baituo@huanleguang.com>
2018-07-13 13:07:44 +0800

    fix: scale double size img

--- src/utils/main.js
+++ src/utils/main.js
@@ -6,13 +6,13 @@ let imgCtx = imgCanvas.getContext('2d');
 function loadImg() {
   let img = new Image()
   img.onload = () => {
-    let w = img.width
-    let h = img.height
+    let w = img.width * 2
+    let h = img.height * 2
 
     imgCtx.clearRect(0, 0, imgCanvas.width, imgCanvas.height)
     imgCanvas.width = w
     imgCanvas.height = h
-    imgCtx.drawImage(img, 0, 0)
+    imgCtx.drawImage(img, 0, 0, w, h)
 
     let pen = new Pen()
     pen.reset()
