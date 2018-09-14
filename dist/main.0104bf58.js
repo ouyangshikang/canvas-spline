ec1dd3a1fc78c44aa0cc8e74772a430e5024a898
baituo <baituo@gaoding.com>
2018-09-14 17:36:43 +0800

    fix: change dist

--- dist/main.0104bf58.js
+++ dist/main.0104bf58.js
@@ -722,7 +722,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '60164' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '61091' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
